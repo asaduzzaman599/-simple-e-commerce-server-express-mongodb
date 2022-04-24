c1f469a81dd2e802ac816f5c8d9ca974a106ddc4
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-04-24 16:40:41 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -2,12 +2,44 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express()
 const port = process.env.PORT || 4000
 
 //middleware
 app.use(cors())
 app.use(express.json())
+
+//mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.waslu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const run = async () => {
+    try {
+        await client.connect()
+        const connectionDB = client.db('emajohn').collection('product')
+
+        app.get('/product', async (req, res) => {
+            const query = {}
+            const result = await connectionDB.find(query).toArray();
+            res.send(result)
+        })
+    }
+    finally { }
+
+
+}
+
+run().catch(console.dir)
+
+
+/* client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log("DB Connected")
+    client.close();
+});
+ */
 
 app.get('/', (req, res) => {
     res.send(`Server Running Port : ${port}`)
