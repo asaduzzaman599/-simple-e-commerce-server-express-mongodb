e74cd82e5781c33968c3613588405289091b5e28
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-04-24 16:23:43 +0600

    init project

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+
+const app = express()
+const port = process.env.PORT || 4000
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send(`Server Running Port : ${port}`)
+})
+
+app.listen(port, () => {
+    console.log(`Server Running Port : ${port}`)
+})
